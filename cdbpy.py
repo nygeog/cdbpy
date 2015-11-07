b10c306cdde594da76ba058851c72fef09d0285a
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-11-06 18:16:05 -0800

    update

--- cdbpy.py
+++ cdbpy.py
@@ -1,5 +1,24 @@
+import urllib
+import urllib2
+import time
 
 def printTest(inText):
 	print inText + ' cdbpy.py - printTest'
+
+def createTable(table_name,username,apikey): #add a field for a dictionary of fields and field types you want to add
+	url = "https://%s.cartodb.com/api/v1/sql" % username
+	insertList = ["CREATE TABLE "+table_name+" (uid int);",
+	"SELECT cdb_cartodbfytable('"+table_name+"');"]
+	for pauser, insert in enumerate(insertList): 
+		params = {'api_key' : apikey,'q' : insert}  
+	    	print insert
+	    	req = urllib2.Request(url, urllib.urlencode(params))
+	    	response = urllib2.urlopen(req)
+	    	print '-' * (pauser + 1)
+	    	time.sleep(2)
+    
+# def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):  	
+# 	"CREATE TABLE latlngtablebuffer_"+fd+" AS SELECT ST_Buffer(the_geom_webmercator, "+bufDist+") AS the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
+#     "SELECT cdb_cartodbfytable('latlngtablebuffer_"+fd+"');",
 
 #def intersect(intables,outtables,infields):
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import _secret_info
 import cdbpy
 
 print 'The test begins now'
 
+cdbU = _secret_info.cartoDBusername
+cdbK = _secret_info.cartoDBapikey
+
 cdbpy.printTest("Let's try the")
+
+cdbpy.createTable("createTableTEST996",cdbU,cdbK)
