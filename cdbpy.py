492f1434601de518ac9b39def3ea5b731af7673e
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-11-08 10:06:56 -0800

    update

--- cdbpy.py
+++ cdbpy.py
@@ -1,3 +1,8 @@
+ ##   ###   ###    ###  #   #
+#     #  #  #  #   #  #  # #
+#     #  #  ###    ###    #
+#     #  #  #  #   #      #
+ ##   ###   ###    #      #
 import urllib
 import urllib2
 import time
@@ -16,7 +21,9 @@ def createTable(table_name,username,apikey): #add a field for a dictionary of fi
 	    	response = urllib2.urlopen(req)
 	    	print '-' * (pauser + 1)
 	    	time.sleep(2)
-    
+  
+#def addPoints(table_name):
+
 # def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):  	
 # 	"CREATE TABLE latlngtablebuffer_"+fd+" AS SELECT ST_Buffer(the_geom_webmercator, "+bufDist+") AS the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
 #     "SELECT cdb_cartodbfytable('latlngtablebuffer_"+fd+"');",
