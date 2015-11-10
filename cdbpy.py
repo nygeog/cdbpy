aef5dfaeee894d312c4b6b87fd7b22e22c8d08ee
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-11-10 08:20:20 -0500

    update

--- cdbpy.py
+++ cdbpy.py
@@ -21,11 +21,27 @@ def createTable(table_name,username,apikey): #add a field for a dictionary of fi
 	    	response = urllib2.urlopen(req)
 	    	print '-' * (pauser + 1)
 	    	time.sleep(2)
-  
+ 
 #def addPoints(table_name):
 
-# def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):  	
-# 	"CREATE TABLE latlngtablebuffer_"+fd+" AS SELECT ST_Buffer(the_geom_webmercator, "+bufDist+") AS the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
-#     "SELECT cdb_cartodbfytable('latlngtablebuffer_"+fd+"');",
+def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):  	
+	url = "https://%s.cartodb.com/api/v1/sql" % username
+	insertList = ["CREATE TABLE "+ou_buffer+" AS SELECT ST_Buffer(the_geom_webmercator, "+buffer_dist+") AS * FROM "+in_buffer, #the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
+    "SELECT cdb_cartodbfytable('"+ou_buffer+"');"]
+    for pauser, insert in enumerate(insertList): 
+		params = {'api_key' : apikey,'q' : insert}  
+	    	print insert
+	    	req = urllib2.Request(url, urllib.urlencode(params))
+	    	response = urllib2.urlopen(req)
+	    	print '-' * (pauser + 1)
+	    	time.sleep(2)
+
+
+#def project():
+
+#def
+
+
+
 
 #def intersect(intables,outtables,infields):
