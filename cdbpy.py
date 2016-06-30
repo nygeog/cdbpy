4cd154b3d7eb5a2d31d63305b8a45b490bcd7178
Danny S <danielmsheehan@iMac.home>
2016-06-30 06:23:41 -0400

    update

--- cdbpy.py
+++ cdbpy.py
@@ -44,4 +44,15 @@ def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):
 
 
 
-#def intersect(intables,outtables,infields):
+def intersect(in_tables,ou_table,in_fields,username,apikey):
+	url = "https://%s.cartodb.com/api/v1/sql" % username
+	insertList = ["CREATE TABLE "+ou_buffer+" AS SELECT ST_Intersection("+in_tables[0]".the_geom, "+in_tables[1]+".the_geom) AS * FROM "+in_buffer, #the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
+    "SELECT cdb_cartodbfytable('"+ou_buffer+"');"]
+	for pauser, insert in enumerate(insertList): 
+		params = {'api_key' : apikey,'q' : insert}  
+	    	print insert
+	    	req = urllib2.Request(url, urllib.urlencode(params))
+	    	response = urllib2.urlopen(req)
+	    	print '-' * (pauser + 1)
+	    	time.sleep(2)
+
