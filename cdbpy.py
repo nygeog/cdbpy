07132827b5f0dfacfd01db64aa565525f113f333
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2016-12-01 11:07:03 -0500

    u[date

--- cdbpy.py
+++ cdbpy.py
@@ -26,27 +26,20 @@ def createTable(table_name,username,apikey): #add a field for a dictionary of fi
 
 def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey):  	
 	url = "https://%s.cartodb.com/api/v1/sql" % username
-	insertList = ["CREATE TABLE "+ou_buffer+" AS SELECT ST_Buffer(the_geom_webmercator, "+buffer_dist+") AS * FROM "+in_buffer, #the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
-    "SELECT cdb_cartodbfytable('"+ou_buffer+"');"]
+	print url
+	insertList = ["CREATE TABLE "+ou_buffer+" AS SELECT ST_Buffer(the_geom_webmercator, "+buffer_dist+") FROM "+in_buffer, """SELECT cdb_cartodbfytable('"""+ou_buffer+"""');"""]
 	for pauser, insert in enumerate(insertList): 
 		params = {'api_key' : apikey,'q' : insert}  
 	    	print insert
 	    	req = urllib2.Request(url, urllib.urlencode(params))
 	    	response = urllib2.urlopen(req)
 	    	print '-' * (pauser + 1)
-	    	time.sleep(2)
-
-
-#def project():
-
-#def
-
-
+	    	time.sleep(5)
 
 
 def intersect(in_tables,ou_table,in_fields,username,apikey):
 	url = "https://%s.cartodb.com/api/v1/sql" % username
-	insertList = ["CREATE TABLE "+ou_buffer+" AS SELECT ST_Intersection("+in_tables[0]".the_geom, "+in_tables[1]+".the_geom) AS * FROM "+in_buffer, #the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
+	insertList = ["CREATE TABLE "+ou_table+" AS SELECT ST_Intersection("+in_tables[0]+".the_geom, "+in_tables[1]+".the_geom) AS * FROM "+in_buffer, #the_geom_webmercator, cartodb_id, cdbawcensusuid FROM latlngtable_"+fd+"",
     "SELECT cdb_cartodbfytable('"+ou_buffer+"');"]
 	for pauser, insert in enumerate(insertList): 
 		params = {'api_key' : apikey,'q' : insert}  
--- test.py
+++ test.py
@@ -6,6 +6,9 @@ print 'The test begins now'
 cdbU = _secret_info.cartoDBusername
 cdbK = _secret_info.cartoDBapikey
 
-cdbpy.printTest("Let's try the")
+#cdbpy.printTest("Let's try the")
 
-cdbpy.createTable("NEWTABLE20151119",cdbU,cdbK)
+#cdbpy.createTable("NEWTABLE20151119",cdbU,cdbK)
+
+cdbpy.buffer("nyc_properties_1","1","nyc_properties_1_buf4",cdbU,cdbK)
+#def buffer(in_buffer,buffer_dist,ou_buffer,username,apikey)
